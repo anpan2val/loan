9307786c7ede98d2a2e3db86e429a216ce8c601b
anpan2val <steganographi+github@gmail.com>
2023-01-08 11:06:32 +0000

    loan module受信後のメッセージ処理

--- x/loan/keeper/msg_server_request_loan.go
+++ x/loan/keeper/msg_server_request_loan.go
@@ -11,7 +11,39 @@ func (k msgServer) RequestLoan(goCtx context.Context, msg *types.MsgRequestLoan)
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Handling the message
-	_ = ctx
+	
+	// Create a new Loan with the following user input
+	var loan = types.Loan{
+		Amount: 	msg.Amount,
+		Fee:		msg.Fee,
+		Collateral: msg.Collateral,
+		Deadline:	msg.Deadline,
+		State:		"requested",
+		Borrower:	msg.Creator,
+	}
+	
+	// TODO: Collateral has to be more than the amount (+fee?)
+	
+	// Get the borrower address
+	borrower, _ := sdk.AccAddressFromBech32(msg.Creator)
+	
+	// Get the collateral as sdk.Coins
+	collateral, err := sdk.ParseCoinsNormalized(laon.Collateral)
+	if err =! nil {
+		panic(err)
+	}
+	
+	// Use the module account as escrow account
+	sdkError := k.bankKeeper.SendCoinsFromAccountToModule(ctx, borrower, types.ModuleName, collateral)
+	if sdkError != nil {
+		return nil, sdkError
+	}
+	
+	// Add the loan to the keeper
+	k.AppendLoan(
+		ctx,
+		loan,
+	)
 
 	return &types.MsgRequestLoanResponse{}, nil
 }
